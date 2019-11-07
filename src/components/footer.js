599cfece4ca401760ec922351a531b39ade4bd1d
Dennis M <46210760+dennismarks@users.noreply.github.com>
2019-11-07 13:05:53 -0500

    Footer updates (#26)
    * Add navigation among money components
    
    * Add progress bar component
    
    * Add progress bar style and update navigation buttons style
    
    * Refactor

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,17 +6,15 @@ export class Footer extends Component {
   render() {
     // It will only show our buttons if it's the right route for them to appear.
     const { location } = this.props;
-    const previousHidden = location => {
+    const showButton = location => {
       switch (location.pathname) {
         case "/concerns":
-          return false;
-        default:
-          return true;
-      }
-    };
-    const nextHidden = location => {
-      switch (location.pathname) {
-        case "/concerns":
+        case "/rent":
+        case "/utilities":
+        case "/deposits":
+        case "/dates":
+        case "/payment":
+        case "/timeline":
           return false;
         default:
           return true;
@@ -26,6 +24,18 @@ export class Footer extends Component {
       switch (location.pathname) {
         case "/concerns":
           return "/disclaimer";
+        case "/rent":
+          return "/concerns";
+        case "/utilities":
+          return "/rent";
+        case "/deposits":
+          return "/utilities";
+        case "/dates":
+          return "/deposits";
+        case "/payment":
+          return "/dates";
+        case "/timeline":
+          return "/payment";
         default:
           return "/";
       }
@@ -34,8 +44,49 @@ export class Footer extends Component {
       switch (location.pathname) {
         case "/concerns":
           return "/rent";
+        case "/rent":
+          return "utilities";
+        case "/utilities":
+          return "/deposits";
+        case "/deposits":
+          return "/dates";
+        case "/dates":
+          return "/payment";
+        case "/payment":
+          return "/timeline";
         default:
           return "/";
+      }
+    };
+    const progressBarHidden = location => {
+      switch (location.pathname) {
+        case "/rent":
+        case "/utilities":
+        case "/deposits":
+        case "/dates":
+        case "/payment":
+        case "/timeline":
+          return false;
+        default:
+          return true;
+      }
+    };
+    const progressUpdate = location => {
+      switch (location.pathname) {
+        case "/rent":
+          return 0;
+        case "/utilities":
+          return (100 / 12) * 1;
+        case "/deposits":
+          return (100 / 12) * 2;
+        case "/dates":
+          return (100 / 12) * 3;
+        case "/payment":
+          return (100 / 12) * 4;
+        case "/timeline":
+          return (100 / 12) * 5;
+        default:
+          return 0;
       }
     };
     return (
@@ -43,14 +94,27 @@ export class Footer extends Component {
         <Link to={previousLink(location)}>
           <button
             id="previous"
-            className={previousHidden(location) ? "hidden" : null}
+            className={showButton(location) ? "hidden" : null}
           >
             <ChevronLeft size={24} className="chevronLeft" />
             Previous
           </button>
         </Link>
+        <div
+          className={progressBarHidden(location) ? "hidden" : "track fade-in"}
+        >
+          <div
+            className="thumb"
+            style={{ width: progressUpdate(location) + "%" }}
+          ></div>
+          <div className="perc-sign">
+            {progressBarHidden(location)
+              ? ""
+              : Math.floor(progressUpdate(location)) + "%"}
+          </div>
+        </div>
         <Link to={nextLink(location)}>
-          <button id="next" className={nextHidden(location) ? "hidden" : null}>
+          <button id="next" className={showButton(location) ? "hidden" : null}>
             Next <ChevronRight size={24} className="chevronRight" />
           </button>
         </Link>
