69c95f3f5a845750c4929fd3904c9b3237da6a22
mousav26 <amousavi@me.com>
2019-11-26 13:51:29 -0500

    Remove server form root

--- app.py
+++
@@ -1,4 +0,0 @@
-from lease_wizard import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- lease_wizard/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-from lease_wizard.config import config
-
-app = Flask(__name__, static_folder='../client/build')
-app.config.from_object(config['development'])
-CORS(app)
-
-from lease_wizard.controller import index
