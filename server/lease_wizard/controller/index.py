8c6d5490fb4745d82da09c6be4d88e83c7e28ebb
Demetre <demetre.jouras@gmail.com>
2019-11-29 20:22:40 -0500

    Mongo DB database (#68)
    * added mongo connection string
    
    * set up connection
    
    * two routes now
    
    * ops
    
    * json dumps output
    
    * turned into one route with methods
    
    * test get returns all leases
    
    * patch for lease working
    
    * polished code to use HTTP verbs
    
    * removed print statements
    
    * removed un needed python module

--- server/lease_wizard/controller/index.py
+++ server/lease_wizard/controller/index.py
@@ -1,6 +1,55 @@
 from lease_wizard import app
 import os
 from flask import send_from_directory
+from flask_pymongo import PyMongo
+from flask import Flask
+import pymongo
+from flask import request
+import json
+import datetime
+
+# TODO: make this dynamically chose beteen local and hosted DB URI
+mongo = pymongo.MongoClient('mongodb+srv://csc301:csc301@cluster0-bzlks.mongodb.net/test?retryWrites=true&w=majority')
+db = mongo["database"]
+# leases refers to the leases collection
+# can also use mongo.db.leases instead
+collection = db["leases"]
+
+
+# can use for debugging
+@app.route('/test')
+def test():
+    return json.dumps("Connection to database")
+
+
+# updates the current lease
+@app.route('/lease', methods=['PATCH', 'GET'])
+def update_lease():
+
+    # returns current lease in database
+    if request.method == 'GET':
+
+        cursor = collection.find({})
+
+        all_leases = []
+        for lease in cursor:
+            temp = {"rent":lease['rent'], "rent_period": lease['rent_period']}
+            all_leases.append(temp)
+
+        return json.dumps(all_leases)
+
+
+    # updates current lease by merging with request body
+    if request.method == 'PATCH':
+        # parses input body into json
+        body = request.json
+
+        lease = collection.find_one_and_update({}, {'$set': body}, {'upsert': True})
+
+        # issues json serializing the _id value of lease
+        return json.dumps("nice")
+
+
 
 
 @app.route('/', defaults={'path': ''})
